f60f84521cd4e4113c714e3d0e4d2718aa0ddb7a
ednukros <isanmenarguez@gmail.com>
2023-02-07 14:24:47 +0100

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule)}
+  { path: '', loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule)},
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule)},
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then((m) => m.AboutModule)}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/contact/contact.module.ts
+++ src/app/pages/contact/contact.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from '../contact/contact.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { AddContactComponent } from './pages/add-contact/add-contact.component';
+import { ContactDetailComponent } from './pages/contact-detail/contact-detail.component';
 
 
 @NgModule({
   declarations: [
-    ContactComponent
+    ContactComponent,
+    ListadoComponent,
+    AddContactComponent,
+    ContactDetailComponent
   ],
   imports: [
     CommonModule,
